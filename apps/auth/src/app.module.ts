bc95b03f7806a7f08676e1da93489aaa8ec2cb58
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2022-09-28 17:18:04 -0500

    adding auth module

---
+++ apps/auth/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
